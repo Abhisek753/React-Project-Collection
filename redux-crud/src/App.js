30a96050be42e2137bb9e547e7df21e940afbe76
Abhisek753 <properachievement@gmail.com>
2024-06-02 01:34:04 +0530

    added crud setup

---
+++ redux-crud/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <p className='p-4 font-bold  text-teal-400'>CRUD APP USING REDUX  (REACT REDUX)</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
